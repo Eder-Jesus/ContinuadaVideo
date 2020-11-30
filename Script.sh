1927e64c3c49c9f1288efb5e59b8eaffd71e757c
eder-jesus <eder.jesus@bandtec.com.br>
2020-11-30 17:36:14 -0200

    Teste

--- Script.sh
+++ Script.sh
@@ -3,14 +3,14 @@
 which java | grep /bin/java
 
 if [[ $? = 0 ]]
-    then
-    echo "Maquina ja possui o Java"
-    java -jar continuada3-1.0-SNAPSHOT-jar-with-dependencies
+   then
+   echo "Maquina ja possui o Java"
+   java -jar continuada3-1.0-SNAPSHOT-jar-with-dependencies
 else
-    sudo apt install curl
-    curl -s "https://get.sdkman.io/" | bash
-    source "$HOME/.sdkman/bin/sdkman-init.sh"
-    sdk install java 8.0.272.j9-adpt
-    echo "Execuntando a Aplicação"
-    java -jar projeto-ac3-1.0-SNAPSHOT-jar-with-dependencies.jar
+   sudo apt install curl
+   curl -s "https://get.sdkman.io/" | bash
+   source "$HOME/.sdkman/bin/sdkman-init.sh"
+   sdk install java 8.0.272.j9-adpt
+   echo "Execuntando a Aplicação"
+   java -jar continuada3-1.0-SNAPSHOT-jar-with-dependencies
 fi
